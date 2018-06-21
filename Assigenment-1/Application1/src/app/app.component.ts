af97eaf031144fc1dc440d727b4f2121986607bf
SagarJadhav <jadhavsagar281@gmail.com>
2018-06-22 01:25:18 +0530

    Assigenment1

---
+++ Assigenment-1/Application1/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root1',
+  templateUrl: './app.component.html',
+  //template: '<div style="text-align:center"><h1>Marvellous Systems</h1><h2>Education for better tomorrow</h2></div>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '';
+  user = 'Marvellous Systems';
+}
